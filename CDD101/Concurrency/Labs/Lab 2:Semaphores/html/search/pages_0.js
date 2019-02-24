309450c404b4c5654168adbef7fef5b3b7aff545
Darran Gahan <d.m.gahan@gmail.com>
2019-02-24 17:06:32 +0000

    Lab2

---
+++ CDD101/Concurrency/Labs/Lab 2:Semaphores/html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['code_20to_20demonstrate_20the_20use_20of_20semaphores',['Code to demonstrate the use of semaphores',['../index.html',1,'']]]
+];
--- CDD101/Concurrency/Labs/Lab 2:Semaphores/main.cpp
+++ CDD101/Concurrency/Labs/Lab 2:Semaphores/main.cpp
@@ -4,7 +4,7 @@
 #include <unistd.h>
 /*! \class Signal
     \brief An Implementation of Threads Using Semaphores 
-
+    \mainpage Code to demonstrate the use of semaphores
    Uses C++11 features such as mutex and condition variables to implement Semaphores in thread functions 
 
 */
@@ -15,9 +15,14 @@ void taskOne(std::shared_ptr<Semaphore> theSemaphore, int delay){
   std::cout << "must ";
   std::cout << "print ";
   std::cout << "first"<<std::endl;
+  /*! taskOne signal when it is finished to allw taskTwo to run*/
+  theSemaphore->Signal();
 }
 /*! displays a message second*/
 void taskTwo(std::shared_ptr<Semaphore> theSemaphore){
+
+  /*! taskTwo is forced to wait until taskOne is finished*/
+  theSemaphore->Wait();
   std::cout <<"This ";
   std::cout << "will ";
   std::cout << "appear ";
