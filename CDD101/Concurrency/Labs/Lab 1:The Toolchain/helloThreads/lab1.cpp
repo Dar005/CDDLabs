23af66742cda185ba45b27f5be93a97f53261d1e
Darran Gahan <d.m.gahan@gmail.com>
2019-02-24 16:52:21 +0000

    Lab1

---
+++ CDD101/Concurrency/Labs/Lab 1:The Toolchain/helloThreads/lab1.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*! \class lab1
+  \brief A lab1 practice lab
+
+  run a loop from 1 to 10 and output lopp counter each loop
+
+*/
+
+
+int main(void){
+
+  int i;
+  for(i=0;i<10;i++)
+    printf("%d",i);
+
+}
